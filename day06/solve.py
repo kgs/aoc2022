612835dbc10d432078690b0d5c877aa30cef1510
Kamil Gorlo <kgorlo@gmail.com>
2022-12-06 09:33:25 +0100

    Day 06 - without deque

--- day06/solve.py
+++ day06/solve.py
@@ -1,19 +1,15 @@
-from collections import deque
-
-
 def solve(input_txt: str, n: int) -> int:
     with open(input_txt) as f:
         s = f.read()
-        d = deque(s[:n])
-        for i, c in enumerate(s[n:]):
-            d.popleft()
-            d.append(c)
-            if len(set(d)) == n:
-                return i + n + 1
+        for i in range(n, len(s)):
+            if len(set(s[i - n:i])) == n:
+                return i
 
 
 if __name__ == "__main__":
+    sample_ans = solve("sample.txt", 4)
+    assert sample_ans == 5
     p1_ans = solve("input.txt", 4)
-    print(f"part1: {p1_ans}")
-    p1_ans = solve("input.txt", 14)
-    print(f"part1: {p1_ans}")
+    assert p1_ans == 1987
+    p2_ans = solve("input.txt", 14)
+    assert p2_ans == 3059
