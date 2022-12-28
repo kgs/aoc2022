52f30e172a885c44bd2061a1ede91c038188d424
Kamil Gorlo <kgorlo@gmail.com>
2022-12-28 22:04:35 +0100

    Day 16 - brute force

---
+++ day16/solve.py
@@ -0,0 +1,86 @@
+from dataclasses import dataclass, field
+from itertools import permutations
+from datetime import datetime
+import math
+import re
+
+INF = 1_000_000_000
+
+
+@dataclass
+class Graph:
+    size: int = 0
+    valve2num: dict[str, int] = field(default_factory=dict)
+    flow_rate: list[int] = field(default_factory=list)
+    neighbours: list[list[int]] = field(default_factory=list)
+    dist: list[list[int]] = field(init=False)
+
+    def get_valve_num(self, valve: str):
+        if valve not in self.valve2num:
+            self.valve2num[valve] = self.size
+            self.size += 1
+            self.flow_rate.append(0)
+            self.neighbours.append([])
+        return self.valve2num[valve]
+
+    def add_node(self, src_node: str, flow_rate: int, dst_nodes: list[str]):
+        x = self.get_valve_num(src_node)
+        self.flow_rate[x] = flow_rate
+        self.neighbours[x] = [self.get_valve_num(node_str) for node_str in dst_nodes]
+
+    def calc_dist(self):
+        n = self.size
+        dist = [[INF] * n for _ in range(n)]
+        for i in range(n):
+            for j in self.neighbours[i]:
+                dist[i][j] = 1
+        for i in range(n):
+            dist[i][i] = 0
+        for k in range(n):
+            for i in range(n):
+                for j in range(n):
+                    x = dist[i][k] + dist[k][j]
+                    if dist[i][j] > x:
+                        dist[i][j] = x
+        self.dist = dist
+
+
+def part1(input_txt: str) -> int:
+    with open(input_txt) as f:
+        g = Graph()
+        lines = map(lambda v: v.strip(), f.readlines())
+        for line in lines:
+            match = re.match(r"Valve ([A-Z]+) has flow rate=(\d+); tunnels? leads? to valves? ([A-Z, ]+)", line)
+            src_node, flow_rate, dst_nodes = match.group(1), int(match.group(2)), match.group(3).split(", ")
+            g.add_node(src_node, flow_rate, dst_nodes)
+        g.calc_dist()
+        nodes = sorted([(g.flow_rate[i], i) for i in range(g.size) if g.flow_rate[i] > 0], reverse=True)
+        best = 0
+        a, b = 0, math.factorial(len(nodes))
+        for nodes_permutation in permutations(nodes):
+            a += 1
+            if a % 10_000_000 == 0:
+                print("{} {:.4f}% {}".format(datetime.now(), a * 100 / b, best))
+            curr_node = 0
+            minutes = 30
+            tmp = 0
+            for fr, i in nodes_permutation:
+                # move there and open valve
+                minutes -= (g.dist[curr_node][i] + 1)
+                if minutes <= 0:
+                    break
+                tmp += fr * minutes
+                curr_node = i
+            best = max(best, tmp)
+        return best
+
+
+if __name__ == "__main__":
+    sample_p1_ans = part1("sample.txt")
+    print(f"sample1: {sample_p1_ans}")
+    p1_ans = part1("input.txt")
+    print(f"part1: {p1_ans}")
+    # sample_p2_ans = part2("sample.txt")
+    # print(f"sample2: {sample_p2_ans}")
+    # p2_ans = part2("input.txt")
+    # print(f"part2: {p2_ans}")
